6b64d58af4ee454d3d238cba36376eb7aa25b937
Breith <b.breith@gmail.com>
2017-06-19 01:13:33 +0200

    Add get parameter from config.yml by Twig

---
+++ Twig/Extension/ParameterExtension.php
@@ -0,0 +1,50 @@
+<?php
+/******************************************************************************
+ * Copyright (c) 2017 Cropper. All rights reserved.                           *
+ * Author      : Breith Barbot                                                *
+ * Updated at  : 18/06/17 23:04                                               *
+ * File name   : ParameterExtension.php                                       *
+ * Description :                                                              *
+ ******************************************************************************/
+
+namespace Breithbarbot\CropperBundle\Twig\Extension;
+
+class ParameterExtension extends \Twig_Extension
+{
+    /**
+     * @var $container
+     */
+    public $container;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('breithbarbot_cropper_parameter', [$this, 'getParameter']),
+        ];
+    }
+
+    /**
+     * @param $name
+     *
+     * @return mixed
+     */
+    public function getParameter($name)
+    {
+        if (!empty($name)) {
+            return $this->container->getParameter($name);
+        }
+
+        return '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'breithbarbot_cropper_parameter';
+    }
+}
