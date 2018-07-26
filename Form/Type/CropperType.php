9e86d4fde1c92afb8d57a8fd5f951018162872bd
Breith Barbot <b.breith@gmail.com>
2018-07-27 01:05:07 +0200

    Fix CS

--- Form/Type/CropperType.php
+++ Form/Type/CropperType.php
@@ -14,8 +14,6 @@ namespace Breithbarbot\CropperBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CropperType extends AbstractType
