80fd28480c6df05dde261a3c7ce8da8b1cdca9c0
Breith Barbot <b.breith@gmail.com>
2020-01-09 01:28:36 +0100

    chore: indent code

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,38 +15,38 @@ use RuntimeException;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
+/**
+ * @see https://symfony.com/doc/current/configuration/using_parameters_in_dic.html
+ */
 class Configuration implements ConfigurationInterface
 {
     /**
      * {@inheritdoc}
-     *
-     * @throws RuntimeException
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('breithbarbot_cropper');
+        $treeBuilder = new TreeBuilder('breithbarbot_cropper');
 
-        $rootNode
+        $treeBuilder->getRootNode()
             ->children()
-            ->arrayNode('mappings')
-            ->useAttributeAsKey('id')
-            ->prototype('array')
-            ->children()
-            ->arrayNode('routes')
-            ->isRequired()
-            ->children()
-            ->scalarNode('path_add')->isRequired()->cannotBeEmpty()->end()
-            ->scalarNode('path_delete')->end()
-            ->end()
-            ->end()
-            ->scalarNode('width')->defaultValue(1280)->end()
-            ->scalarNode('height')->defaultValue(720)->end()
-            ->scalarNode('ratio')->defaultValue('16/9')->end()
-            ->end()
-            ->end()
-            ->end()
-            ->end()
+                ->arrayNode('mappings')
+                    ->useAttributeAsKey('id')
+                        ->prototype('array')
+                            ->children()
+                                ->arrayNode('routes')
+                                    ->isRequired()
+                                    ->children()
+                                        ->scalarNode('path_add')->isRequired()->cannotBeEmpty()->end()
+                                        ->scalarNode('path_delete')->end()
+                                    ->end()
+                                ->end()
+                                ->scalarNode('width')->defaultValue(1280)->end()
+                                ->scalarNode('height')->defaultValue(720)->end()
+                                ->scalarNode('ratio')->defaultValue('16/9')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
             ->end()
         ;
 
